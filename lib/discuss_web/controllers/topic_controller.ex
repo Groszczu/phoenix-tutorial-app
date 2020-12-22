a2a80e526f3b2e97949d40f7f3a19b6946736324
Roch Goszczyński <roch.goszczynski@gmail.com>
2020-12-22 21:46:27 +0100

    Add edit and delete endpoints [#86-93]

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -27,4 +27,35 @@ defmodule DiscussWeb.TopicController do
         render(conn, "new.html", changeset: changeset)
     end
   end
+
+  def edit(conn, %{"id" => topic_id}) do
+    topic = Repo.get!(Topic, topic_id)
+    changeset = Topic.changeset(topic, %{})
+
+    render(conn, "edit.html", changeset: changeset, topic: topic)
+  end
+
+  def update(conn, %{"id" => topic_id, "topic" => topic}) do
+    old_topic = Repo.get!(Topic, topic_id)
+    changeset = Topic.changeset(old_topic, topic)
+
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Updated")
+        |> redirect(to: Routes.topic_path(conn, :index))
+
+      {:error, changeset} ->
+        render(conn, "edit.html", changeset: changeset, topic: old_topic)
+    end
+  end
+
+  def delete(conn, %{"id" => topic_id}) do
+    Repo.get!(Topic, topic_id)
+    |> Repo.delete!()
+
+    conn
+    |> put_flash(:info, "Topic Deleted")
+    |> redirect(to: Routes.topic_path(conn, :index))
+  end
 end
--- lib/discuss_web/router.ex
+++ lib/discuss_web/router.ex
@@ -17,9 +17,7 @@ defmodule DiscussWeb.Router do
     pipe_through :browser
 
     get "/", TopicController, :index
-    get "/topics", TopicController, :index
-    get "/topics/new", TopicController, :new
-    post "/topics", TopicController, :create
+    resources "/topics", TopicController
   end
 
   # Other scopes may use custom stacks.
