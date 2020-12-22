0aefd57d9c5f3d5e2fa22ea08575fdafdd413d37
Roch Goszczyński <roch.goszczynski@gmail.com>
2020-12-22 20:17:37 +0100

    Add link to new topic form [#85]

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -18,8 +18,10 @@ defmodule DiscussWeb.TopicController do
     changeset = Topic.changeset(%Topic{}, topic)
 
     case Repo.insert(changeset) do
-      {:ok, struct} ->
-        IO.inspect(struct)
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: Routes.topic_path(conn, :index))
 
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
